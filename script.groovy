513fbc976bae914910cd695987a54fa22b83b7ab
mrnegmat <mr.negmat@gmail.com>
2023-02-13 11:02:28 +0500

    groovy

--- script.groovy
+++ script.groovy
@@ -8,11 +8,11 @@ def buildImage(){
     sh "docker build -t projectvprofile/java-maven-app:jma-2.1 ."  
 }
 
-// def pushImage(){
-//     withCredentials([usernamePassword(credentialsId: "cred-docker-hub", passwordVariable: "PASSWORD", usernameVariable: "USERNAME")]){
-//         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"                
-//         sh "docker push projectvprofile/java-maven-app:jma-2.1"        
-//     }    
-// }
+def pushImage(){
+    withCredentials([usernamePassword(credentialsId: "cred-docker-hub", passwordVariable: "PASSWORD", usernameVariable: "USERNAME")]){
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"                
+        sh "docker push projectvprofile/java-maven-app:jma-2.1"        
+    }    
+}
 
 return this
